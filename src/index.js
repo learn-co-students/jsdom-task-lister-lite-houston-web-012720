aed6f19aa3389c63b18536e6c6027d5d4afeb1d5
DrVonDevious <drvondevious@gmail.com>
2020-03-10 14:15:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const text = document.querySelector("#new-task-description");
+  const submit = document.querySelector("#submit-button");
+  const list = document.querySelector("#tasks");
+  const form = document.querySelector("#create-task-form");
+
+  submit.addEventListener("click", () => {
+    event.preventDefault();
+    var new_item = document.createElement("li");
+    new_item.innerHTML = text.value;
+
+    var delete_task = document.createElement("input");
+    delete_task.type = "submit";
+    delete_task.value = "Delete";
+
+    list.append(new_item);
+    list.append(delete_task);
+
+    delete_task.addEventListener("click", () => {
+      event.preventDefault();
+      new_item.remove();
+    });
+
+  });
+
 });
