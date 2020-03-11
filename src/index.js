4ca7bfc4b98777d5d9b635718fc991587e4b9bef
GianMoran <gpmoran@uh.edu>
2020-03-10 20:23:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   let taskForm = document.querySelector('#create-task-form');
+
+  let select = document.createElement('select');
+  select.id = "priority"
+  let option = document.createElement('option');
+  let option2 = document.createElement('option');
+  let option3 = document.createElement('option');
+
+  option.value = "red"
+  option.innerText = "high"
+  // option.id = "red"
+  option2.value = "yellow"
+  option2.innerText = "medium"
+  // option2.id = "yellow"
+  option3.value = "green"
+  option3.innerText = "low"
+  // option3.id = "green"
+
+
+  select.append(option);
+  select.append(option2);
+  select.append(option3);
+
+  taskForm.append(select);
+
   taskForm.addEventListener("submit", ()=>{
     event.preventDefault();
     const input = document.querySelector('#new-task-description');
     input.value
-    let ul = document.querySelector('ul#tasks')
-    let li = document.createElement('li')
- 
-    li.innerText = input.value
-    let button = document.createElement('button')
+    const priority = document.querySelector('#priority')
+    let ul = document.querySelector('ul#tasks');
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    // debugger
+    li.style.color = priority.value;
+    let button = document.createElement('button');
     button.innerHTML = "delete";
-    li.setAttribute("id",input.value)
+    li.setAttribute("id",input.value);
     li.append(button);
     ul.append(li);
 
@@ -18,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
     deleteTask();
 
   });
-    
+
   });
   const deleteTask = () => {
     const input = document.querySelector('#new-task-description');
